34d1217fdf6105dfe2cc33accf9a515a744efd25
andre <tiltedaf3@gmail.com>
2022-06-29 00:28:59 +0800

    Display result on test dataset after training

--- test.py
+++ test.py
@@ -14,12 +14,12 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 Neural network 1.0
 SGD + backprop using sigmoid function
 """
-##net = network.Network([784, 30, 10])
-##net.SGD(training_data, 30, 10, 3.13, validation_data = validation_data)
-##test_data = list(test_data)
-##total_test = len(test_data)
-##results = 100.0 * net.evaluate(test_data, convert=True)/total_test
-##print(f"Accuracy on test data: {results}%")
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 0.5, validation_data = validation_data)
+test_data = list(test_data)
+total_test = len(test_data)
+results = 100.0 * net.evaluate(test_data)/total_test
+print(f"Accuracy on test data: {results}%")
 
 
 """
@@ -28,19 +28,19 @@ Similar to 1.0, with the addition of
 a) L2 regularization
 b) cross-entropy function
 c) better initialization of weights
-"""
-net = network_2.Network([784, 30, 10], cost=network_2.CrossEntropyCost)
-net.SGD(training_data, 2, 10, 0.4,
-        reg_param = 5.0,
-        eval_data = validation_data,
-        monitor_eval_cost=False,
-        monitor_eval_acc=True,
-        monitor_trng_cost=False,
-        monitor_trng_acc=False)
-test_data = list(test_data)
-total_test = len(test_data)
-results = 100.0 * net.accuracy(test_data, convert=False)/total_test
-print(f"Accuracy on test data: {results}%")
+##"""
+##net = network_2.Network([784, 30, 10], cost=network_2.CrossEntropyCost)
+##net.SGD(training_data, 30, 10, 0.4,
+##        reg_param = 5.0,
+##        eval_data = validation_data,
+##        monitor_eval_cost=False,
+##        monitor_eval_acc=True,
+##        monitor_trng_cost=False,
+##        monitor_trng_acc=False)
+##test_data = list(test_data)
+##total_test = len(test_data)
+##results = 100.0 * net.accuracy(test_data, convert=False)/total_test
+##print(f"Accuracy on test data: {results}%")
 
 
 
